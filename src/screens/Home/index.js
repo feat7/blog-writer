9de2d37746eb40db7be04da4ec95aeedfa0f5e44
Vinay Khobragade <vinay@localhost.localdomain>
2018-07-07 02:28:53 +0530

    add: editor

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
 
+import { Editor, createEditorState } from "medium-draft";
+
 @inject("store")
 @observer
 export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorState: createEditorState() // for empty content
+    };
+
+    this.onChange = editorState => {
+      this.setState({ editorState });
+    };
+  }
+
+  componentDidMount() {
+    this.refs.editor.focus();
+  }
+
   render() {
-    return <div>Hello World</div>;
+    return (
+      <Editor
+        ref="editor"
+        editorState={this.state.editorState}
+        onChange={this.onChange}
+      />
+    );
   }
 }
