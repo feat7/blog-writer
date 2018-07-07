3ab1e33199e266c88ef6e8ac0cbdc139ca32c455
Kuldeep Pisda <pisdak79@gmail.com>
2018-07-07 19:42:25 +0530

    Updated with bulma

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -38,6 +38,7 @@ export default class HomeScreen extends Component {
       // this.setState({ editorState: createEditorState(JSON.parse(editorData)) });
     };
 
+    this.clearTopTen = this.clearTopTen.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.suggestKeywords = this.suggestKeywords.bind(this);
     this.saveBlog = this.saveBlog.bind(this);
@@ -62,20 +63,8 @@ export default class HomeScreen extends Component {
           createEditorState(JSON.parse(data))
             .getCurrentContent()
             .getPlainText(" ")
-<<<<<<< HEAD
-        )
-      );
-
-      // this.setState({ editorState: createEditorState(JSON.parse(editorData)) });
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.suggestKeywords = this.suggestKeywords.bind(this);
-    this.clearTopTen = this.clearTopTen.bind(this);
-=======
         );
       });
->>>>>>> d4f48d59c2dc042e0448e58e44b45f9102268bbd
   }
 
   handleChange(event) {
@@ -145,197 +134,83 @@ export default class HomeScreen extends Component {
   }
 
   render() {
-<<<<<<< HEAD
-    var keywordsList = this.state.topTenKeywords.map((keyword, index) =>
-      <li key={index}>
-=======
     var keywordsList = this.state.topTenKeywords.map((keyword, index) => (
-      <li className="list-group-item" key={index}>
->>>>>>> d4f48d59c2dc042e0448e58e44b45f9102268bbd
+      <li key={index}>
         {keyword.keyword}
       </li>
     ));
     return (
       <div>
-<<<<<<< HEAD
-        <div className="columns">
-          <div className="column is-two-thirds">
-            <div className="hero">
-              <div className="hero-body">
-                <div className="columns">
-                  <div className="column is-10 has-text-centered">
-                    <div className="container">
+        <div className="hero">
+          <div className="hero-body">
+            <div className="columns">
+              <div className="column has-text-centered">
+                <div className="container">
+                  <div className="columns">
+                    <div className="column is-two-thirds">
                       <div className="content">
                         <Editor
                           ref="editor"
                           editorState={this.state.editorState}
                           onChange={this.onChange}
                         />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="column" style={{ borderLeftWidth: 1 }}>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="column is-one-third">
-            <div className="sideNavigator">
-              <div className="card">
-                <div className="card-content">
-                  <div className="box">
-                    Word Count: {this.state.wordCount}
-                  </div>
-                  <div className="box">
-                    Find Keywords
-                    <div className="input-group mb-3">
-                      <input name="keyword" type="text" className="form-control" placeholder="Type a Keyword" aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={this.handleChange} />
-                      <div className="input-group-append">
-                        <button className="button btn-outline-secondary" type="button" onClick={this.suggestKeywords}>
-                          Search
+                        <button
+                          className="button is-primary is-large is-rounded"
+                          onClick={this.saveBlog}
+                        >
+                          Save
                         </button>
                       </div>
                     </div>
-                    Total Suggested Keywords: {this.state.suggestedKeywords.length}
-                    <br />
-                    <p>Filter Keywords on the basis of <button type="button" className="button btn-outline-warning" onClick={this.clearTopTen} >Clear</button></p>
-                    <div className="columns">
-                      <div className="column">
-                        <div className="form-check">
-                          <input className="form-check-input" type="radio" name="filterMode" id="cpc" value="cpc" defaultChecked onChange={this.handleChange} />
-                          <label className="form-check-label">
-                            CPC
-                          </label>
-                        </div>
+                    <div className="column is-one-third">
+                      <div className="column" style={{ borderLeftWidth: 1 }}>
+                          <div className="box">
+                              Word Count:{" "} { this.state.editorState .getCurrentContent() .getPlainText(" ") .split(" ").length }
+                          </div>
+                          <div className="box">
+                              Find Keywords
+                              <div className="field is-grouped">
+                                  <div className="control">
+                                      <input name="keyword" type="text" className="input" placeholder="Type a Keyword" aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={this.handleChange} />
+                                  </div>
+                                  <div className="control">
+                                      <button className="button is-primary" type="button" onClick={this.suggestKeywords}>
+                                          Search
+                                      </button>
+                                  </div>
+                              </div>
+                              Total Suggested Keywords:{" "} {this.state.suggestedKeywords.length}
+                              <button className="button is-primary" type="button" onClick={this.clearTopTen}>
+                                Clear Results
+                              </button>
+                              <br />
+                              <p>Filter Keywords on the basis of  </p>
+                              <div className="columns">
+                                  <div className="column is-one-quarter">
+                                      <div className="form-check">
+                                          <input className="form-check-input" type="radio" name="filterMode" id="cpc" value="cpc" defaultChecked onChange={this.handleChange} />
+                                          <label className="form-check-label">CPC</label>
+                                      </div>
+                                  </div>
+                                  <div className="column is-one-quarter">
+                                      <div className="form-check">
+                                          <input className="form-check-input" type="radio" name="filterMode" id="volume" value="Volume" onChange={this.handleChange} />
+                                          <label className="form-check-label">Volume</label>
+                                      </div>
+                                  </div>
+                                  <div className="column is-half">
+                                      <div className="form-check">
+                                          <input className="form-check-input" type="radio" name="filterMode" id="competition" value="Competition" onChange={this.handleChange} />
+                                          <label className="form-check-label">Competition</label>
+                                      </div>
+                                  </div>
+                              </div>
+                              <br />
+                              <ol>{keywordsList}</ol>
+                          </div>
                       </div>
-                      <div className="column">
-                        <div className="form-check">
-                          <input className="form-check-input" type="radio" name="filterMode" id="volume" value="Volume" onChange={this.handleChange} />
-                          <label className="form-check-label">
-                            Volume
-                          </label>
-                        </div>
-                      </div>
-                      <div className="column is-half">
-                        <div className="form-check">
-                          <input className="form-check-input" type="radio" name="filterMode" id="competition" value="Competition" onChange={this.handleChange} />
-                          <label className="form-check-label">
-                            Competition
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                    <br />
-                    <div className="container">
-                      <ol>
-                        {keywordsList}
-                      </ol>
-                    </div>
-=======
-        <div className="hero">
-          <div className="hero-body">
-            <div className="columns">
-              <div className="column has-text-centered">
-                <div className="container">
-                  <div className="content">
-                    <Editor
-                      ref="editor"
-                      editorState={this.state.editorState}
-                      onChange={this.onChange}
-                    />
-                    <button
-                      className="button is-primary is-large is-rounded"
-                      onClick={this.saveBlog}
-                    >
-                      Save
-                    </button>
                   </div>
-                </div>
-              </div>
-              <div className="column" style={{ borderLeftWidth: 1 }}>
-                <div className="box">
-                  Word Count:{" "}
-                  {
-                    this.state.editorState
-                      .getCurrentContent()
-                      .getPlainText(" ")
-                      .split(" ").length
-                  }
-                </div>
-                <div className="box">
-                  Find Keywords
-                  <div className="field is-grouped">
-                    <div className="control">
-                      <input
-                        name="keyword"
-                        type="text"
-                        className="input"
-                        placeholder="Type a Keyword"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                        onChange={this.handleChange}
-                      />
-                    </div>
-                    <div className="control">
-                      <button
-                        className="button is-primary"
-                        type="button"
-                        onClick={this.suggestKeywords}
-                      >
-                        Search
-                      </button>
-                    </div>
                   </div>
-                  Total Suggested Keywords:{" "}
-                  {this.state.suggestedKeywords.length}
-                  <br />
-                  <p>Filter Keywords on the basis of </p>
-                  <div className="row">
-                    <div className="col-4">
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="filterMode"
-                          id="cpc"
-                          value="cpc"
-                          defaultChecked
-                          onChange={this.handleChange}
-                        />
-                        <label className="form-check-label">CPC</label>
-                      </div>
-                    </div>
-                    <div className="col-4">
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="filterMode"
-                          id="volume"
-                          value="Volume"
-                          onChange={this.handleChange}
-                        />
-                        <label className="form-check-label">Volume</label>
-                      </div>
-                    </div>
-                    <div className="col-4">
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="filterMode"
-                          id="competition"
-                          value="Competition"
-                          onChange={this.handleChange}
-                        />
-                        <label className="form-check-label">Competition</label>
-                      </div>
-                    </div>
->>>>>>> d4f48d59c2dc042e0448e58e44b45f9102268bbd
-                  </div>
-                  <br />
-                  <ul className="list-group">{keywordsList}</ul>
                 </div>
               </div>
             </div>
@@ -345,3 +220,4 @@ export default class HomeScreen extends Component {
     );
   }
 }
+
